058863f91653c20b0f4c5d931efd8df5323c14de
Gavin Elder <gavin@improbable.io>
2019-03-02 23:54:06 +0000

    Break need to implement time check fully.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# puppet-nanny
+# Puppet-Nanny
 
 [![Go Report Card](https://goreportcard.com/badge/github.com/gavinelder/puppet-nanny)](https://goreportcard.com/report/github.com/gavinelder/puppet-nanny)
 
 :warning: The following is not complete
+
--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func checkDisabled() {
 
 	lockfilelocation := "/opt/puppetlabs/puppet/cache/state/agent_disabled.lock"
 	if _, err := os.Stat(lockfilelocation); err == nil {
-		// path/to/whatever exists
 		println("Diabled Lock found we don't like diabled run")
 		os.Remove(lockfilelocation)
 		fmt.Println("Lock file removed")
@@ -23,16 +24,24 @@ func checkDisabled() {
 func checkLockFile() {
 
 	lockfilelocation := "/opt/puppetlabs/puppet/cache/state/agent_catalog_run.lock"
-	if _, err := os.Stat(lockfilelocation); err == nil {
-		// path/to/whatever exists
+	if filestat, err := os.Stat(lockfilelocation); err == nil {
+		now = time.time()
+		if filestat.ModTime()
 		println("Run lock found removing")
 		os.Remove(lockfilelocation)
 		fmt.Println("Lock file removed")
+	} else {
+		println("No run lock found proceeding")
 	}
+}
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
 }
 
 func runPuppet() {
-
+	checkDisabled()
+	checkLockFile()
 	cmd := exec.Command("/opt/puppetlabs/puppet/bin/puppet", "agent", "-t")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -47,8 +56,8 @@ func runPuppet() {
 }
 
 func main() {
-	checkDisabled()
-	checkLockFile()
+	myrand := random(5, 30)
+	fmt.Printf("Delaying puppet-nanny run by %d minutes", myrand)
+	time.Sleep(time.Duration(myrand) * time.Minute)
 	runPuppet()
-
 }
