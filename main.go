23c0565927366341493b9f93752da5f5f7b723c9
Gavin Elder <gavin@improbable.io>
2019-03-02 22:46:09 +0000

    Strip back

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
-import "github.com/GavinElder/puppet-nanny/cmd"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func checkDisabled() {
+	fileStat, err := os.Stat("/opt/puppetlabs/puppet/bin/puppet")
+
+	if err != nil {
+		log.Fatal("File does not exist")
+	}
+	fileStat.ModTime()
+}
+
+func checkLockFile() {
+
+}
 
 func main() {
-	cmd.Execute()
+	checkDisabled()
+	checkLockFile()
+
+	cmd := exec.Command("/opt/puppetlabs/puppet/bin/puppet", "agent", "-t")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+
 }
