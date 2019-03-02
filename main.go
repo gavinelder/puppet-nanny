8396b4f89396c539402f829fcabef64fd0e27712
Gavin Elder <gavin@improbable.io>
2019-03-02 23:08:52 +0000

    Add basic checks for lock files.

--- main.go
+++ main.go
@@ -9,21 +9,29 @@ import (
 )
 
 func checkDisabled() {
-	fileStat, err := os.Stat("/opt/puppetlabs/puppet/bin/puppet")
 
-	if err != nil {
-		log.Fatal("File does not exist")
+	lockfilelocation := "/opt/puppetlabs/puppet/cache/state/agent_disabled.lock"
+	if _, err := os.Stat(lockfilelocation); err == nil {
+		// path/to/whatever exists
+		println("Diabled Lock found we don't like diabled run")
+		os.Remove(lockfilelocation)
+		fmt.Println("Lock file removed")
 	}
-	fileStat.ModTime()
+
 }
 
 func checkLockFile() {
 
+	lockfilelocation := "/opt/puppetlabs/puppet/cache/state/agent_catalog_run.lock"
+	if _, err := os.Stat(lockfilelocation); err == nil {
+		// path/to/whatever exists
+		println("Run lock found removing")
+		os.Remove(lockfilelocation)
+		fmt.Println("Lock file removed")
+	}
 }
 
-func main() {
-	checkDisabled()
-	checkLockFile()
+func runPuppet() {
 
 	cmd := exec.Command("/opt/puppetlabs/puppet/bin/puppet", "agent", "-t")
 	var stdout, stderr bytes.Buffer
@@ -35,5 +43,12 @@ func main() {
 	}
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+
+}
+
+func main() {
+	checkDisabled()
+	checkLockFile()
+	runPuppet()
 
 }
