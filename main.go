4ce4377698b87108d9fc40712d457b54490ca112
Gavin <gav.elder@gmail.com>
2019-05-26 14:53:00 +0100

    General Quality of life improvements.
    * General Cleanup
    
    * Clearer var names.
    
    * Handle logic a bit cleaner
    
    * Update comments.
    
    * Improve errors.
    
    * A little cleaner.
    
    * Remove unused func.
    
    * Clean up log messages.
    
    * GoFumpt.

--- main.go
+++ main.go
@@ -9,114 +9,98 @@ import (
 	"time"
 )
 
-func checkDisabled() {
-	//Check for a diabled lockfile and if present remove to allow puppet to run.
-	lockfilelocation := ""
-	switch os := runtime.GOOS; os {
-	case "darwin", "linux":
-		lockfilelocation = "/opt/puppetlabs/puppet/cache/state/agent_disabled.lock"
-	case "windows":
-		lockfilelocation = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_disabled.lock"
+func removeAgentDisableLock(disableLockFileLocation string) error {
+	// Check for a diabled lockfile and if present remove to allow puppet to run.
+	if _, err := os.Stat(disableLockFileLocation); err == nil {
+		log.Printf("Disable lock found, removing.\n")
+		if err := os.Remove(disableLockFileLocation); err != nil {
+			log.Printf("Unable to remove lockfile %s\n", disableLockFileLocation)
+			return err
+		}
+		log.Printf("Lock file removed.\n")
+		return nil
 	}
-	if _, err := os.Stat(lockfilelocation); err == nil {
-		log.Println("Disabl lock found, removing")
-		os.Remove(lockfilelocation)
-		log.Println("Lock file removed")
-	}
-
+	return nil
 }
 
-func checkPuppetInstalled() {
-	//Check for the puppet binary.
-	puppetBinLocation := ""
-	switch os := runtime.GOOS; os {
-	case "darwin", "linux":
-		puppetBinLocation = "/opt/puppetlabs/puppet/bin/puppet"
-	case "windows":
-		puppetBinLocation = "C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat"
-	default:
-		log.Fatalf("Err %s not supported.", os)
-	}
-
-	log.Printf("Puppet binary found at %s", puppetBinLocation)
-
-	if _, err := os.Stat(puppetBinLocation); err != nil {
-		log.Fatalf("Puppet binary not found at %s", puppetBinLocation)
-	}
-
-}
-
-func checkLockFile() {
-	//Check for puppet run lock & remove if > 25 mins old.
-	lockfilelocation := ""
-	switch os := runtime.GOOS; os {
-	case "darwin", "linux":
-		lockfilelocation = "/opt/puppetlabs/puppet/cache/state/agent_catalog_run.lock"
-	case "windows":
-		lockfilelocation = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_catalog_run.lock"
-	}
-	if filestat, err := os.Stat(lockfilelocation); err == nil {
-
+func checkRunState(runLockFileLocation string) error {
+	// Check for puppet running state by inspecting the lockfile & remove if > 25 mins old.
+	if filestat, err := os.Stat(runLockFileLocation); err == nil {
 		now := time.Now()
 		cutoff := 25 * time.Minute
 		if diff := now.Sub(filestat.ModTime()); diff > cutoff {
-			log.Printf("Deleting %s which is %s old\n", filestat.Name(), diff)
-			os.Remove(lockfilelocation)
+			if err := os.Remove(runLockFileLocation); err != nil {
+				log.Fatalf("Unable to remove lockfile %s.\n", runLockFileLocation)
+			}
+			log.Printf("Deleting %s which is %s old.\n", filestat.Name(), diff)
 		} else {
-			log.Printf("Found lock file %s which is less than %s old aborting run\n", filestat.Name(), cutoff)
-			runPuppet()
+			log.Printf("Found lock file %s which is less than %s old aborting run.\n", filestat.Name(), cutoff)
+			return err
 		}
-
 	} else {
-		log.Printf("No run lock found proceeding")
+		log.Printf("No run lock found proceeding. \n")
 	}
+	return nil
 }
+
 func random(min, max int) int {
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(max-min) + min
 }
 
-func runPuppet() {
-	//Run puppet.
+func runPuppet(puppetBinLocation, execPuppetCMD, runLockFileLocation, disableLockFileLocation string) {
+	if _, err := os.Stat(puppetBinLocation); err != nil {
+		log.Fatalf("Puppet binary not found at %s. \n", puppetBinLocation)
+	}
+	// Sleep until we need to run
 	myrand := random(15, 45)
 	log.Printf("Delaying puppet-nanny run by %d minutes", myrand)
 	time.Sleep(time.Duration(myrand) * time.Minute)
-	checkDisabled()
-	checkLockFile()
-	checkPuppetInstalled()
-	cmd := exec.Command("")
-	switch os := runtime.GOOS; os {
-	case "darwin", "linux":
-		cmd = exec.Command("/opt/puppetlabs/puppet/bin/puppet", "agent", "-t")
-	case "windows":
-		cmd = exec.Command("C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat", "agent", "-t")
+	// Carry out necessary checks to see if we should run puppet at this time.
+	if err := checkRunState(runLockFileLocation); err != nil {
+		return
 	}
+	// Check if puppet is disabled, checked as part of every run as someone may disable at any point.
+	if err := removeAgentDisableLock(disableLockFileLocation); err != nil {
+		return
+	}
+	cmd := exec.Command(execPuppetCMD)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	log.Println("Running Puppet")
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("cmd.Run() failed with %s.\n", err)
 	}
-	runPuppet()
-}
-
-func sanityChecks() {
-	//Ensure puppet is running with elevated privledge.
-	switch OS := runtime.GOOS; OS {
-	case "darwin", "linux":
-		if os.Getuid() != 0 {
-			log.Fatalf("puppet-nanny needs to be ran as root:")
-		}
-	case "windows":
-		_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
-		if err != nil {
-			log.Fatalf("puppet-nanny needs to be ran as Admin:")
-		}
-	}
-	runPuppet()
+	log.Printf("Puppet run succeeded.\n")
 }
 
 func main() {
-	sanityChecks()
+	execPuppetCMD := ""
+	runLockFileLocation := ""
+	disableLockFileLocation := ""
+	puppetBinLocation := ""
+
+	switch goos := runtime.GOOS; goos {
+	case "darwin", "linux":
+		if os.Getuid() != 0 {
+			log.Fatalf("puppet-nanny needs to be ran as root")
+		}
+		puppetBinLocation = "/opt/puppetlabs/puppet/bin/puppet"
+		execPuppetCMD = `"/opt/puppetlabs/puppet/bin/puppet", "agent", "-t"`
+		runLockFileLocation = "/opt/puppetlabs/puppet/cache/state/agent_catalog_run.lock"
+		disableLockFileLocation = "/opt/puppetlabs/puppet/cache/state/agent_disabled.lock"
+	case "windows":
+		if _, err := os.Open("\\\\.\\PHYSICALDRIVE0"); err != nil {
+			log.Fatalf("puppet-nanny needs to be ran with admin privledges. \n")
+		}
+		puppetBinLocation = "C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat"
+		execPuppetCMD = `"C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat", "agent", "-t"`
+		runLockFileLocation = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_catalog_run.lock"
+		disableLockFileLocation = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_disabled.lock"
+	default:
+		log.Fatalf("OS not supported.\n")
+	}
+	log.Printf("Puppet binary set as %s.\n", puppetBinLocation)
+	for {
+		runPuppet(puppetBinLocation, execPuppetCMD, runLockFileLocation, disableLockFileLocation)
+	}
 }
